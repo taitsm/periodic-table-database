c56f6422049d005c9220593b77521f1b88797135
Tainã Martinez <tai.souza.martinez@gmail.com>
2024-07-14 20:36:40 +0000

    feat: add scripts into element.sh

--- element.sh
+++ element.sh
@@ -2,12 +2,28 @@
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+else
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+  # find element by its atomic_number
+    QUERY="SELECT elements.atomic_number, elements.name, elements.symbol, types.type, properties.atomic_mass, properties.melting_point_celsius, properties.boiling_point_celsius FROM elements INNER JOIN properties ON elements.atomic_number=properties.atomic_number INNER JOIN types ON properties.type_id=types.type_id WHERE elements.atomic_number=$1"
+  else
+  # find element by its Symbol or Name
+    QUERY="SELECT elements.atomic_number, elements.name, elements.symbol, types.type, properties.atomic_mass, properties.melting_point_celsius, properties.boiling_point_celsius FROM elements INNER JOIN properties ON elements.atomic_number=properties.atomic_number INNER JOIN types ON properties.type_id=types.type_id WHERE elements.symbol='$1' OR elements.name='$1'"
+  fi
+
+  ELEMENT_INFO=$($PSQL "$QUERY")
+
+  if [[ -z $ELEMENT_INFO ]]
+  then
   # if no element found
-
-  # if element found
-
-  # return element by its atomic_number
-
-  # return element by its Symbol
-
-  # return element by its Name
+    echo "I could not find that element in the database."
+  else
+  # return element info
+    IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING_POINT BOILING_POINT <<< "$ELEMENT_INFO"
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+  fi
+fi

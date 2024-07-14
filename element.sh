2bc0d34232585491ded8ed1a1af6f59d9046cbdc
Tainã Martinez <tai.souza.martinez@gmail.com>
2024-07-14 20:33:19 +0000

    chore: add commnets to element.sh script

--- element.sh
+++ element.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+  # if no element found
+
+  # if element found
+
+  # return element by its atomic_number
+
+  # return element by its Symbol
+
+  # return element by its Name
